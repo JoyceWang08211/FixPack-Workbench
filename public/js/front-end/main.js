df4f9828a9fc57a543428f39787df8ada4e82635
haoliang.wu <haoliang.wu@liferay.com>
2015-11-11 13:31:33 +0800

    SF

--- public/js/front-end/main.js
+++ public/js/front-end/main.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var $ = require('jquery');
 
 $(document).ready(function () {
@@ -10,7 +11,7 @@ $(document).ready(function () {
             }
         });
 
-        React.render(<ContentBox/>, document.getElementById('_content'));
+        ReactDOM.render(<ContentBox/>, document.getElementById('_content'));
     }
 )
 
