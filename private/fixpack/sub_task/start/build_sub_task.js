b8588b224a059d6bc673c4481a68eb9a993bf25e
haoliang.wu <haoliang.wu@liferay.com>
2015-12-07 12:58:43 +0800

    实现build sub task脚本中bind lpe和lps的功能

--- private/fixpack/sub_task/start/build_sub_task.js
+++ private/fixpack/sub_task/start/build_sub_task.js
@@ -10,14 +10,14 @@ const fixPackInfo = properties.getFixPackInfo();
 const user = properties.getUserInfo();
 
 describe('Fix Pack Build Sub Tasks Script', function () {
-    let JobTitle;
     let LPECache;
     let LPSCache;
+    let SubTaskCache;
 
     this.timeout(1000 * 60 * 60);
 
     before(function* () {
-        yield browser.url('https://issues.liferay.com/browse/LRQA-19810')
+        yield browser.url(`https://issues.liferay.com/browse/${fixPackInfo.ticket}`)
             .setValue('#login-form-username', user.username)
             .setValue('#login-form-password', user.password);
         let title = yield browser.click('#login-form-submit').getTitle();
@@ -88,32 +88,85 @@ describe('Fix Pack Build Sub Tasks Script', function () {
             })
         );
 
-        console.log(LPSCache);
+        if (LPSCache.length != 0)
+            console.log(`${LPSCache.toString()} need to be created..`);
+        else
+            console.log(`Nothing needs to be created..`)
     });
 
     it('Build Sub Task..', function*() {
-        for (let item of LPSCache.entries) {
+        for (let item of LPSCache.entries()) {
             const LPE = item[0];
             const LPS = item[1];
+
+            yield browser.click('//div[@class="command-bar"]/div/div/div/ul[3]/li[2]/div/a')
+                .pause(3000)
+                .click('//div[@class="aui-list"]/ul[5]/li[1]/a')
+                .pause(3000)
+                .setValue('#issuetype-field', 'Sub-Task')
+                .pause(3000)
+                .keys('\uE007')
+
+                .setValue('#summary', `${fixPackInfo.name}: ${LPE}(${LPS})`)
+
+                .setValue('#components-textarea', 'Fix Pack Testing')
+                .pause(3000)
+                .keys('\uE007')
+
+                .click('#create-issue-submit')
+                .pause(3000);
         }
-
-        yield browser.click('//div[@class="command-bar"]/div/div/div/ul[3]/li[2]/div/a')
-            .pause(3000)
-            .click('//div[@class="aui-list"]/ul[5]/li[1]/a')
-            .pause(3000)
-            .setValue('#issuetype-field', 'Sub-Task')
-            .pause(3000)
-            .keys('\uE007')
-
-            .setValue('#summary', 12345)
-
-            .setValue('#components-textarea', 'Fix Pack Testing')
-            .pause(3000)
-            .keys('\uE007')
-
-            .click('#create-issue-submit')
-            .pause(3000);
     });
 
+    it('Catch Sub Task Link List..', function*() {
+        const subTasks = yield browser.elements('//table[@id="issuetable"]/tbody/tr/td[2]/a');
+        SubTaskCache = yield subTasks.value.map((st)=> {
+            let id = st.ELEMENT;
+            return browser.elementIdAttribute(id, 'data-issue-key').then((e)=> {
+                return e.value
+            });
+        });
+    });
+
+    it('Bind LPS and LPE to Sub Task..', function *() {
+        let LPEResult, LPSResult, LPE, LPS;
+
+        for (let st of SubTaskCache) {
+            const isExisted = yield browser.url(`https://issues.liferay.com/browse/${st}`)
+                .isExisting('#linkingmodule_heading').then((isExisted)=> {
+                    return isExisted;
+                })
+            console.log(isExisted);
+            if (isExisted)
+                continue;
+            else {
+                let text = yield browser.getText('#summary-val');
+
+                LPEResult = text.match(/LPE-\d*/ig);
+                LPSResult = text.match(/LPS-\d*/ig);
+
+                LPE = LPEResult ? LPEResult[0] : 'LPE-*****';
+                LPS = LPSResult ? LPSResult[0] : 'LPS-*****';
+
+                yield browser
+                    .click('//div[@class="command-bar"]/div/div/div/ul[3]/li[2]/div/a')
+                    .pause(3000)
+                    .click('//div[@class="aui-list"]/ul[6]/li[1]/a')
+                    .pause(3000)
+                    .setValue('#jira-issue-keys-textarea', `${LPE}`)
+                    .pause(3000)
+                    .keys('\uE013')
+                    .keys('\uE004')
+
+                    .setValue('#jira-issue-keys-textarea', `${LPS}`)
+                    .pause(3000)
+                    .keys('\uE013')
+                    .keys('\uE004')
+
+                    .click('//div[@id="link-issue-dialog"]//input[@name="Link"]')
+                    .pause(3000);
+            }
+        }
+    });
 });
 
