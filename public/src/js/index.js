72779fdd35047e4406ea1187deaa0fb363c54b5c
haoliang.wu <haoliang.wu@liferay.com>
2015-12-14 17:41:27 +0800

    使用start bootstrap主题 Grayscale

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,35 +1,34 @@
-//css
-require('../css/style.css');
-
 //js
-const React = require('react');
-const ReactDOM = require('react-dom');
-const $ = require('jquery');
+require('../js/lib/theme/jquery.easing.min');
+//const React = require('react');
+//const ReactDOM = require('react-dom');
+//const $ = require('jquery');
+//
+//$(document).ready(function () {
+//        var ContentBox = React.createClass({
+//            render() {
+//                return (
+//                    <div className="jumbotron">
+//                        <h1>Fix Pack Workbench
+//                            <small> For more accuracy and efficiency</small>
+//                        </h1>
+//                        <p>Crawler For Jenkins Server</p>
+//
+//                        <p><a className="btn btn-primary btn-lg" href="/crawler" role="button">Learn more</a></p>
+//
+//                        <p>Editor For Liferay POSHI</p>
+//
+//                        <p><a className="btn btn-primary btn-lg" href="/editors" role="button">Learn more</a></p>
+//
+//                        <p>CNQA Knowledge Base</p>
+//
+//                        <p><a className="btn btn-primary btn-lg" href="/kb" role="button">Learn more</a></p>
+//                    </div>
+//                )
+//            }
+//        });
+//
+//        ReactDOM.render(<ContentBox/>, document.getElementById('_content'));
+//    }
+//);
 
-$(document).ready(function () {
-        var ContentBox = React.createClass({
-            render() {
-                return (
-                    <div className="jumbotron">
-                        <h1>Fix Pack Workbench
-                            <small> For more accuracy and efficiency</small>
-                        </h1>
-                        <p>Crawler For Jenkins Server</p>
-
-                        <p><a className="btn btn-primary btn-lg" href="/crawler" role="button">Learn more</a></p>
-
-                        <p>Editor For Liferay POSHI</p>
-
-                        <p><a className="btn btn-primary btn-lg" href="/editors" role="button">Learn more</a></p>
-
-                        <p>CNQA Knowledge Base</p>
-
-                        <p><a className="btn btn-primary btn-lg" href="/kb" role="button">Learn more</a></p>
-                    </div>
-                )
-            }
-        });
-
-        ReactDOM.render(<ContentBox/>, document.getElementById('_content'));
-    }
-);
